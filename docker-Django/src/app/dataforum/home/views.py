33b92f0ff13bd42159f408e78226397fa3614f2e
Abelit <ychenid@live.com>
2017-02-17 16:55:57 +0800

    update dataforum/*

--- docker-Django/src/app/dataforum/home/views.py
+++ docker-Django/src/app/dataforum/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse(u"欢迎光临 abelit的网站!")
-    return render(request, 'home.html')
+    today = datetime.datetime.now().date()
+    return render(request, 'home.html',{'today': today})
